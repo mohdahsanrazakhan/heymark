07210ebf67df4347528cddc3205e055329dd4184
Mark <datawithmark@gmail.com>
2024-01-31 22:22:57 +0530

    Updating Hero, Contact, and Footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,17 +2,17 @@ import './Contact.css'
 
 const Contact = () => {
   return (
-    <section className="flex lg:flex-row flex-col gap-10 lg:gap-0 justify-center items-center h-[100vh] lg:mx-28">
-      <div className="flex flex-col w-1/2">
+    <section className="flex lg:flex-row flex-col gap-10 lg:gap-0 justify-center items-center min-h-[100vh] lg:mx-28">
+      <div className="flex flex-col mx-10 sm:m-0 sm:w-1/2">
         <h2 className="text-[3.5rem] md:text-[6rem] w-1/2 landing-none font-[clash-display-semibold]">Let's work together</h2>
         <svg className="arrow m-auto" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="rgb(0, 0, 0)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" style={{ width: '10%', height: '10%' }}>
           <line x1="7" y1="7" x2="17" y2="17"></line>
           <polyline points="17 7 17 17 7 17"></polyline>
         </svg>
-        <a className="email sm:text-[1.5rem] sm:pt-1 pt-3landing-none sm:font-[clash-display-semibold] font-mono sm:font-normal font-semibold ml-auto" href="mailto:khanmohdahsanraza@gmail.com">khanmohdahsanraza@gmail.com</a>
+        <a className="email text-[1.2rem] sm:text-[1.5rem] sm:pt-1 pt-3 landing-none sm:font-[clash-display-semibold] font-mono sm:font-normal font-semibold ml-auto" href="mailto:khanmohdahsanraza@gmail.com">khanmohdahsanraza@gmail.com</a>
       </div>
-      <div className="flex w-1/2 justify-center items-center">
-        <p className="text-[1.2rem] landing-none font-mono w-[25rem] lg:ml-auto m-auto text-center lg:text-left prose ">I'm currently available for new work. Let me know if you're looking for a Front-end Developer. Let's talk about the next big thing!</p>
+      <div className="flex flex-wrap px-10 mb-10 lg:mb-0 sm:px-0 w-full sm:w-1/2 justify-center items-center">
+        <p className="text-[1.2rem] landing-none font-mono w-[25rem] lg:ml-auto m-auto text-center lg:text-left">I'm currently available for new work. Let me know if you're looking for a Front-end Developer. Let's talk about the next big thing!</p>
       </div>
     </section>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,18 +3,21 @@ import './Hero.css'
 const Hero = () => {
   return (
     <section className='flex flex-col flex-wrap justify-center items-center h-screen relative'>
-      <p className='role text-[2rem]'>I'm <span className='circle-sketch-highlight'>front-end developer</span></p>
 
-      <div className="text-[4.5rem] sm:text-[10rem] md:text-[12rem] lg:text-[16rem] xl:text-[20rem] landing-none font-[clash-display-semibold]">
-        <h1 data-shadow="MARK">
-          <span id='M'>M</span>
-          <span id='A'>A</span>
-          <span id='R'>R</span>
-          <span id='K'>K</span>
-        </h1>
+      <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+        <p className='role text-[2rem] text-center'>I'm <span className='circle-sketch-highlight'>front-end developer</span></p>
+
+        <div className="text-[4.5rem] sm:text-[10rem] md:text-[12rem] lg:text-[16rem] xl:text-[20rem] landing-none font-[clash-display-semibold]">
+          <h1 data-shadow="MARK">
+            <span id='M'>M</span>
+            <span id='A'>A</span>
+            <span id='R'>R</span>
+            <span id='K'>K</span>
+          </h1>
+        </div>
       </div>
 
-      <div className='text-center m-auto absolute bottom-10 left-[1/2]'>
+      <div className='text-center m-auto absolute bottom-12 left-[1/2]'>
         <div className='border-2 border-black rounded-xl w-fit m-auto p-1'>
           <p className='animate-bounce font-semibold'>⚫</p>
         </div>
