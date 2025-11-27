09b0b81adc6086ee169c7f8756ea75383907033d
mohdahsanrazakhan <datawithmark@gmail.com>
2025-11-27 13:33:04 +0530

    portfolio projects uploaded

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -8,14 +8,17 @@ const Skill = () => {
       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 justify-center items-center place-content-center w-fit m-auto gap-5'>
         <ToolCard skill="C++" url="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="cplusplus" />
         <ToolCard skill="JavaScript" url="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" />
+        <ToolCard skill="TypeScript" url="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="typescript" />
         <ToolCard skill="MongoDB" url="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" />
         <ToolCard skill="ExpressJS" url="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original.svg" alt="expressjs" />
         <ToolCard skill="ReactJS" url="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="reactjs" />
+        <ToolCard skill="Next.js" url="https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg" alt="nextjs" />
         <ToolCard skill="NodeJS" url="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg" alt="nodejs" />
-        <ToolCard skill="TailwindCSS" url="https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg" alt="tailwindcss" />
+        <ToolCard skill="TailwindCSS" url="https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-original.svg" alt="tailwindcss" />
         <ToolCard skill="Git" url="https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg" alt="git" />
         <ToolCard skill="GitHub" url="https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original.svg" alt="github" />
         <ToolCard skill="Sass" url="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" alt="sass" />
+
       </div>
     </section>
   )
